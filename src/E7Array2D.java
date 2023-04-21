893b75c1f3b6a2d4422ccc4d9d810d1052b841cb
marikgossen <130005202+marikgossen@users.noreply.github.com>
2023-04-21 18:47:53 -0400

    Add files via upload

---
+++ src/E7Array2D.java
@@ -0,0 +1,22 @@
+package class11;
+
+public class E7Array2D {
+    public static void main(String[] args) {
+        boolean[][] arr2D = {
+                {true, false, true, true, false},
+                {true, false, false, false, false}
+        };
+        int sum=0;
+        for (boolean[] booleans : arr2D) {
+            for (boolean aBoolean : booleans) {
+                if(aBoolean){
+                    sum++;
+
+                }
+
+            }
+        }
+        System.out.println(sum);
+
+    }
+}
---
+++ src/StudentTester.java
@@ -0,0 +1,14 @@
+package class11;
+
+import java.util.Scanner;
+
+public class StudentTester {
+    public static void main(String[] args) {
+
+     Student studentHaneefah=new Student();
+        studentHaneefah.sleep();
+
+
+
+    }
+}
